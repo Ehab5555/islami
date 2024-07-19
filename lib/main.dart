0fb0c83a375b30ea4e651aaae28d52c4e94c05d0
Ehab Ahmed <ko840482@gmail.com>
2024-07-19 19:04:08 +0300

    initialize project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:islami/home_screen.dart';
+
+void main() {
+  runApp(IslamiApp());
+}
+
+class IslamiApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomeScreen.routeName: (_) => HomeScreen(),
+      },
+    );
+  }
+}
