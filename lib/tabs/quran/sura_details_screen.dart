36569fd9dd5f754e3879ac57772059965e9393cc
Ehab Ahmed <ko840482@gmail.com>
2024-07-30 15:24:27 +0300

    Update quran tab

--- lib/tabs/quran/sura_details_screen.dart
+++ lib/tabs/quran/sura_details_screen.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:islami/app_theme.dart';
 import 'package:islami/tabs/quran/quran_tab.dart';
 
-class SuraDetailsScreen extends StatelessWidget {
+class SuraDetailsScreen extends StatefulWidget {
   static const String routeName = '/sura_details';
 
-  List<String> ayat = [
-    'بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم',
-    'الْحَمْدُ لِلَّهِ رَبِّ الْعَالَمِينَ',
-    'الرَّحْمَنِ الرَّحِيمِ',
-    'مَالِكِ يَوْمِ الدِّينِ',
-    'إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُ',
-    'اهْدِنَا الصِّرَاطَ الْمُسْتَقِيمَ',
-    'صِرَاطَ الَّذِينَ أَنْعَمْتَ عَلَيْهِمْ غَيْرِ الْمَغْضُوبِ عَلَيْهِمْ وَلَا الضَّالِّين',
-  ];
+  @override
+  State<SuraDetailsScreen> createState() => _SuraDetailsScreenState();
+}
+
+class _SuraDetailsScreenState extends State<SuraDetailsScreen> {
+  List<String> ayat = [];
+  late SuraDetailsArgs args;
+
   @override
   Widget build(BuildContext context) {
-    SuraDetailsArgs args =
-        ModalRoute.of(context)!.settings.arguments as SuraDetailsArgs;
+    args = ModalRoute.of(context)!.settings.arguments as SuraDetailsArgs;
+    loadSuraFile();
     return Container(
       decoration: const BoxDecoration(
         image: DecorationImage(
@@ -29,52 +29,64 @@ class SuraDetailsScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text(args.suraName),
         ),
-        body: Container(
-          padding: EdgeInsets.symmetric(
-              vertical: MediaQuery.of(context).size.height * 0.02),
-          margin: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.height * 0.05,
-              vertical: MediaQuery.of(context).size.width * 0.14),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(
-              Radius.circular(20),
-            ),
-            color: AppTheme.white,
-          ),
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.only(bottom: 12),
+        body: ayat.isEmpty
+            ? const Center(child: CircularProgressIndicator())
+            : Container(
+                padding: EdgeInsets.symmetric(
+                    vertical: MediaQuery.of(context).size.height * 0.02),
+                margin: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.height * 0.05,
+                    vertical: MediaQuery.of(context).size.width * 0.14),
                 decoration: const BoxDecoration(
-                    border: Border(bottom: BorderSide(width: 0.1))),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  color: AppTheme.white,
+                ),
+                child: Column(
                   children: [
-                    Text(
-                      'سورة ${args.suraName}',
-                      style: Theme.of(context).textTheme.headlineSmall,
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 12),
+                      decoration: const BoxDecoration(
+                          border: Border(bottom: BorderSide(width: 0.1))),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'سورة ${args.suraName}',
+                            style: Theme.of(context).textTheme.headlineSmall,
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          const Icon(Icons.play_circle_fill)
+                        ],
+                      ),
                     ),
-                    const SizedBox(
-                      width: 20,
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: ListView.builder(
+                          itemBuilder: (_, index) => Text(
+                            ayat[index],
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                          itemCount: ayat.length,
+                        ),
+                      ),
                     ),
-                    const Icon(Icons.play_circle_fill)
                   ],
                 ),
               ),
-              Expanded(
-                child: ListView.builder(
-                  itemBuilder: (_, index) => Text(
-                    ayat[index],
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  itemCount: ayat.length,
-                ),
-              ),
-            ],
-          ),
-        ),
       ),
     );
+  }
+
+  Future<void> loadSuraFile() async {
+    String sura =
+        await rootBundle.loadString('assets/files/${args.index + 1}.txt');
+    ayat = sura.split('\r\n');
+    setState(() {});
   }
 }
